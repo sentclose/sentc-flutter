bbd4f4c1a5a52373e5a923342a1302842e1d7921
Jörn Heinemann <joernheinemann@gmx.de>
2023-05-29 01:06:55 +0200

    updated flutter rust bridge and flutter sdk

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import sentc
-import shared_preferences_macos
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SentcPlugin.register(with: registry.registrar(forPlugin: "SentcPlugin"))
