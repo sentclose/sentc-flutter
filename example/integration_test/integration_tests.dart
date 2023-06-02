e3685f7c3e0708c9c69cf0f11652573701f2dc86
Jörn Heinemann <joernheinemann@gmx.de>
2023-06-02 18:00:10 +0200

    added group tests part one

--- example/integration_test/integration_tests.dart
+++ example/integration_test/integration_tests.dart
@@ -1,10 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'tests/user_test.dart' as user_tests;
+import 'tests/group_tests_1.dart' as group_tests_1;
 import 'tests/file_test.dart' as file_tests;
 
 void main() {
   group("user tests", user_tests.main);
+
+  group("group tests 1", group_tests_1.main);
 
   group("file tests", file_tests.main);
 }
