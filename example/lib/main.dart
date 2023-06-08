2d40908c4916fd6a292c1f7dcf7f65ea3a7b6327
Jörn Heinemann <joernheinemann@gmx.de>
2023-06-08 14:53:29 +0200

    added simplified example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,141 +1,69 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
 import 'package:sentc/sentc.dart';
-import 'package:sentc_example/core/routes/routes.dart' as routes;
 
-void main() {
-  runApp(const SentcExample());
+void main() async {
+  //init the client to load the native dependency
+  await Sentc.init(appToken: "5zMb6zs3dEM62n+FxjBilFPp+j9e7YUFA+7pi6Hi");
+
+  runApp(const MyApp());
 }
 
-class SentcExample extends StatelessWidget {
-  const SentcExample({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      initialRoute: routes.userR,
-      onGenerateRoute: routes.generateRoutes,
-    );
-  }
-}
-
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Sentc flutter demo",
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: const MyHomepage(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  Future<String>? register;
+class MyHomepage extends StatefulWidget {
+  const MyHomepage({Key? key}) : super(key: key);
 
-  _asyncInit() async {
-    await Sentc.init(
-      appToken: "5zMb6zs3dEM62n+FxjBilFPp+j9e7YUFA+7pi6Hi",
-      baseUrl: "http://192.168.178.21:3002",
-      //baseUrl: "http://127.0.0.1:3002",
-    );
+  @override
+  State<MyHomepage> createState() => _MyHomepageState();
+}
 
-    var reg = await Sentc.register("userIdentifier", "password");
+class _MyHomepageState extends State<MyHomepage> {
+  demo() async {
+    //register a user
+    await Sentc.register("userIdentifier", "password");
 
-    print(reg);
-
-    print("Login user");
+    //log in a user
     final user = await Sentc.login("userIdentifier", "password");
 
-    print("create group");
-
+    //create a group
     final groupId = await user.createGroup();
+
+    //load a group. returned a group obj for every user.
     final group = await user.getGroup(groupId);
 
-    print("delete group");
+    //invite another user to the group. Not here in the example because we only got one user so far
+    // await group.inviteAuto("other user id");
 
+    //encrypt a string for the group
+    final encrypted = await group.encryptString("hello there!");
+
+    //now every user in the group can decrypt the string
+    final decrypted = await group.decryptString(encrypted);
+
+    print(decrypted); //hello there!
+
+    //delete a group
     await group.deleteGroup();
 
-    print("delete user");
+    //delete a user
     await user.deleteUser("password");
-
-    //set here the futures
-    setState(() {
-      print("init");
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    _asyncInit();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (register != null) {
-      //normal view
-
-      return MaterialApp(
-        home: ListViewLayout(
-          <Widget>[
-            FutureBuilder<List<dynamic>>(
-              future: Future.wait([register ?? Future(() => "")]),
-              builder: (context, snap) {
-                final data = snap.data;
-
-                if (data == null) {
-                  return const Text("Loading");
-                }
-
-                return Text(
-                  '${data[0]}',
-                  style: Theme.of(context).textTheme.headline4,
-                );
-              },
-            ),
-          ],
-        ),
-      );
-    } else {
-      //waiting view for init the app
-
-      return const MaterialApp(
-        home: ListViewLayout(
-          <Widget>[Text("waiting")],
-        ),
-      );
-    }
-  }
-}
-
-class ListViewLayout extends StatelessWidget {
-  final List<Widget> body;
-
-  const ListViewLayout(this.body, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Layout(
-      Center(
-        child: ListView(
-          children: body,
-        ),
-      ),
-    );
-  }
-}
-
-class Layout extends StatelessWidget {
-  final Widget body;
-
-  const Layout(this.body, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Plugin example app'),
-      ),
-      body: body,
-    );
+    return const Placeholder();
   }
 }
