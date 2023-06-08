563747d21e360838abb9746f813444f612f5f13a
Jörn Heinemann <joernheinemann@gmx.de>
2023-06-08 14:54:07 +0200

    update pub

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import path_provider_foundation
-import sentc
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SentcPlugin.register(with: registry.registrar(forPlugin: "SentcPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
