fd1b9994181db374de34d36086c0c97673cefc78
Jörn Heinemann <joernheinemann@gmx.de>
2025-05-05 21:48:48 +0200

    update to frb v2

--- sentc_light/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sentc_light/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import sentc_light
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SentcLightPlugin.register(with: registry.registrar(forPlugin: "SentcLightPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
